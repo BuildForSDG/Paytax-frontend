f1939c16c5e8277aaba4b9fefe0c107abe656ea0
sammiepius <sammiepius10@gmail.com>
2020-05-27 09:49:50 +0100

    #fixed1 create a landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -49,55 +49,54 @@ export default class LandingPage extends React.Component {
               <SideNavBar />
             </Col> */}
         <Row className="p-0 m-0 pt-lg-3  d-flex flex-direction-row align-items-center">
-        <Col md="8">
-          <center>
-            <h6 style={{ color: 'red', fontFamily: 'Times New Roman', fontSize: '20px' }}>
-              AN EASY WAY TO MANAGE <br />
-              YOUR TAX
-            </h6>
-          </center>
-          <br />
-          <img className="img-fluid br-3" src={require('./work_together.svg')} />
-        </Col>
-        <Col md="4">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-style-8">
-              <div className="form-control form-control-sm sam">
-                <Icon icon={user} />
-                <input
-                  className="input"
-                  name="tax_id"
-                  type="text"
-                  value={this.state.tax_id}
-                  onChange={this.handleChange}
-                  placeholder="Tax ID"
-                  autoComplete="off"
-                />
+          <Col md="8">
+            <center>
+              <h6 style={{ color: 'red', fontFamily: 'Times New Roman', fontSize: '20px' }}>
+                AN EASY WAY TO MANAGE <br />
+                YOUR TAX
+              </h6>
+            </center>
+            <br />
+            <img className="img-fluid br-3" src={require('./work_together.svg')} />
+          </Col>
+          <Col md="4">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-style-8">
+                <div className="form-control form-control-sm sam">
+                  <Icon icon={user} />
+                  <input
+                    className="input"
+                    name="tax_id"
+                    type="text"
+                    value={this.state.tax_id}
+                    onChange={this.handleChange}
+                    placeholder="Tax ID"
+                  />
+                </div>
+                <br />
+                <div className="form-control form-control-sm sam">
+                  <Icon icon={eye} />
+                  <input
+                    className="input"
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <br />
+                <center>
+                  <button className="btn btn-primary">Login</button>{' '}
+                  <button className="btn btn-primary" type="reset">
+                    Register
+                  </button>
+                </center>
               </div>
-              <br />
-              <div className="form-control form-control-sm sam">
-                <Icon icon={eye} />
-                <input
-                  className="input"
-                  placeholder="Password"
-                  type="password"
-                  name="password"
-                  autoComplete="off"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <br />
-              <center>
-                <button className="btn btn-primary">Login</button>{' '}
-                <button className="btn btn-primary" type="reset">
-                  Register
-                </button>
-              </center>
-            </div>
-          </form>
-        </Col>
-        ;</Row>
+            </form>
+          </Col>
+          ;
+        </Row>
       </div>
     );
   }
