2b64c4fc375170837af0e9c6c65b7c33bfbb957c
Daniel Ufeli <danielufeli@yahoo.com>
2020-06-07 11:03:26 +0100

    Fixes Netlify Issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
