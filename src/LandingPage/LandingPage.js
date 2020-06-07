fd066d11d9ae0a3529b590b5d824112fb5d966c1
Daniel Ufeli <danielufeli@yahoo.com>
2020-06-07 03:53:49 +0100

    Fininshes Auth functionality

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,39 +1,65 @@
-import React from 'react';
-import { Row, Col, Collapse, Navbar, NavbarToggler, NavbarBrand, NavbarText, Nav, Container } from 'reactstrap';
+import React, { Fragment, useState } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import { Row, Col, Collapse, Navbar, NavbarToggler, NavbarBrand, NavbarText, Nav } from 'reactstrap';
 import './index.css';
 import { Icon } from 'react-icons-kit';
 import { eye } from 'react-icons-kit/fa/eye';
 import { user } from 'react-icons-kit/icomoon/user';
-import SideNavBar from '../SideNavBar';
+import { signup, login } from '../actions/auth';
 
-export default class LandingPage extends React.Component {
-  state = {
-    tax_id: '',
+const LandingPage = ({ signup, login, auth }) => {
+  let history = useHistory();
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: '/dashboard' } };
+  const [formData, setFormData] = useState({
+    company: '',
+    name: '',
+    address: '',
+    email: '',
+    maritalStatus: '',
+    phone: '',
+    status: '',
+    gender: '',
+    birthDate: '',
+    bvn: '',
     password: '',
-    landing_page: []
-  };
+    state: '',
+    city: '',
+    taxPayerId: '',
+    passwordLogin: ''
+  });
 
-  handleChange = ({ target }) => {
-    this.setState({
-      [target.name]: target.value
-    });
-  };
+  const { name, address, email, phone, bvn, state, city, taxPayerId, passwordLogin, password } = formData;
 
-  handleSubmit = (e) => {
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.setState(
-      (prevState) => ({
-        landing_page: prevState.landing_page.concat({
-          tax_id: this.state.tax_id,
-          password: this.state.password
-        })
-      }),
-      console.log(this.state.tax_id, this.state.password)
-    );
+    // const formDataSignup = new FormData();
+    // formDataSignup.append('company', company);
+    // formDataSignup.append('name', name);
+    // formDataSignup.append('address', address);
+    // formDataSignup.append('email', email);
+    // formDataSignup.append('maritalStatus', maritalStatus);
+    // formDataSignup.append('phone', phone);
+    // formDataSignup.append('status', status);
+    // formDataSignup.append('gender', gender);
+    signup(formData);
   };
 
-  render() {
-    return (
+  const onSubmit1 = (e) => {
+    e.preventDefault();
+    login(taxPayerId, passwordLogin);
+  };
+
+  if (auth && auth.isAuthenticated) {
+    from ? history.replace(from) : history.replace('/dashboard');
+  }
+
+  return (
+    <Fragment>
       <div>
         <Navbar color="primary" light expand="md">
           <NavbarBrand href="/" style={{ color: 'white' }}>
@@ -49,56 +75,244 @@ export default class LandingPage extends React.Component {
               <SideNavBar />
             </Col> */}
         <Row className="p-0 m-0 pt-lg-3  d-flex flex-direction-row align-items-center">
-        <Col md="8">
-          <center>
-            <h6 style={{ color: 'red', fontFamily: 'Times New Roman', fontSize: '20px' }}>
-              AN EASY WAY TO MANAGE <br />
-              YOUR TAX
-            </h6>
-          </center>
-          <br />
-          <img className="img-fluid br-3" src={require('./work_together.svg')} />
-        </Col>
-        <Col md="4">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-style-8">
-              <div className="form-control form-control-sm sam">
-                <Icon icon={user} />
-                <input
-                  className="input"
-                  name="tax_id"
-                  type="text"
-                  value={this.state.tax_id}
-                  onChange={this.handleChange}
-                  placeholder="Tax ID"
-                  autoComplete="off"
-                />
+          <Col md="8">
+            <center>
+              <h6 style={{ color: 'red', fontFamily: 'Times New Roman', fontSize: '20px' }}>
+                AN EASY WAY TO MANAGE <br />
+                YOUR TAX
+              </h6>
+            </center>
+            <br />
+            <img Alt="" className="img-fluid br-3" src={require('./work_together.svg')} />
+          </Col>
+          <Col md="4">
+            <form onSubmit={(e) => onSubmit1(e)}>
+              <div className="form-style-8">
+                <div className="form-control form-control-sm sam">
+                  <Icon icon={user} />
+                  <input
+                    className="input"
+                    name="taxPayerId"
+                    type="text"
+                    value={taxPayerId}
+                    placeholder="Tax ID"
+                    autoComplete="off"
+                    onChange={(e) => onChange(e)}
+                    required
+                  />
+                </div>
+                <br />
+                <div className="form-control form-control-sm sam">
+                  <Icon icon={eye} />
+                  <input
+                    className="input"
+                    placeholder="Password"
+                    type="password"
+                    name="passwordLogin"
+                    value={passwordLogin}
+                    autoComplete="off"
+                    onChange={(e) => onChange(e)}
+                    required
+                  />
+                </div>
+                <br />
+                <center>
+                  <button className="btn btn-primary">Login</button>{' '}
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                    Register
+                  </button>
+                </center>
               </div>
-              <br />
-              <div className="form-control form-control-sm sam">
-                <Icon icon={eye} />
-                <input
-                  className="input"
-                  placeholder="Password"
-                  type="password"
-                  name="password"
-                  autoComplete="off"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <br />
-              <center>
-                <button className="btn btn-primary">Login</button>{' '}
-                <button className="btn btn-primary" type="reset">
-                  Register
-                </button>
-              </center>
-            </div>
-          </form>
-        </Col>
-        ;</Row>
+            </form>
+          </Col>
+          ;
+        </Row>
       </div>
-    );
-  }
-}
+
+      <div
+        class="modal fade"
+        id="exampleModalLong"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLongTitle"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            {auth && auth.taxid ? (
+              <div class="modal-header text-center">
+                <h5 class="modal-title" id="exampleModalLongTitle">
+                  You have Successfully Registered, your Tax ID is {auth.taxid.PaytaxId}
+                  <br />
+                  Login with your Tax ID and Password to continue to your Dashboard.
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            ) : (
+              <div>
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLongTitle">
+                    Register and take control of your tax payment
+                  </h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <form onSubmit={(e) => onSubmit(e)}>
+                  <div class="modal-body">
+                    <div className="form-group">
+                      <div class="form-check-inline">
+                        <label class="form-check-label">
+                          <input
+                            type="radio"
+                            class="form-check-input"
+                            onChange={(e) => onChange(e)}
+                            name="company"
+                            value="false"
+                          />
+                          Individual
+                        </label>
+                      </div>
+                      <div class="form-check-inline">
+                        <label class="form-check-label">
+                          <input
+                            type="radio"
+                            class="form-check-input"
+                            onChange={(e) => onChange(e)}
+                            name="company"
+                            value="true"
+                          />
+                          Company
+                        </label>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        value={name}
+                        onChange={(e) => onChange(e)}
+                        placeholder="Full Name"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <textarea
+                        name="address"
+                        class="form-control"
+                        value={address}
+                        onChange={(e) => onChange(e)}
+                        cols="30"
+                        rows="3"
+                        placeholder="Address"
+                      ></textarea>
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        value={email}
+                        onChange={(e) => onChange(e)}
+                        placeholder="Email"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="number"
+                        className="form-control"
+                        name="phone"
+                        value={phone}
+                        onChange={(e) => onChange(e)}
+                        placeholder="Phone no"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="number"
+                        className="form-control"
+                        name="bvn"
+                        value={bvn}
+                        onChange={(e) => onChange(e)}
+                        placeholder="BVN"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => onChange(e)}
+                        className="form-control"
+                        placeholder="Password"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="password"
+                        name="confirmPassword"
+                        onChange={(e) => onChange(e)}
+                        className="form-control"
+                        placeholder="Confirm Password"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="text"
+                        name="state"
+                        value={state}
+                        onChange={(e) => onChange(e)}
+                        className="form-control"
+                        placeholder="State"
+                        required
+                      />
+                    </div>
+                    <div class="form-group">
+                      <input
+                        type="text"
+                        name="city"
+                        value={city}
+                        onChange={(e) => onChange(e)}
+                        className="form-control"
+                        placeholder="City"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                      Close
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+LandingPage.protoTypes = {
+  signup: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { signup, login })(LandingPage);
---
+++ src/actions/auth.js
@@ -0,0 +1,124 @@
+import Axios from 'axios';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+    LOGOUT,
+  CLEAR_PROFILE
+} from './types';
+import setAuthToken from '../Utils/setAuthToken';
+import { setAlert } from './alert';
+
+const url = 'http://localhost:8000/api/v1/user';
+
+export const loadUser = () => async dispatch => {
+  
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }else{
+    console.log('no token')
+  }
+  const loadUserApi = `${url}/:taxID`;
+  
+  try {
+    const config = {
+      headers: {
+        'Cache-Control': 'no-cache',
+      }
+    };
+    const res = await Axios.get(loadUserApi, config);
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data.data
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
+
+// Signup User
+
+export const signup = (formData) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  try {
+    const registerApi = `${url}/register`;
+    const res = await Axios.post(registerApi, formData, config);
+    console.log(res.data)
+    if(res.data.status === true){
+      dispatch(setAlert('You have Successfully Registered', 'success'));
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+    } else{
+      dispatch(setAlert(res.data[0], 'error'));
+    }
+  } catch (err) {
+    console.log(err)
+    // setTimeout(() => {
+    //   dispatch(window.location.reload())
+    // }, 3000); 
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
+}; 
+
+// Login User
+
+export const login = (taxPayerId, password) => async dispatch => {
+  const loginApi = `${url}/login`;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({taxPayerId, password });
+
+  try {
+    const res = await Axios.post(loginApi, body, config);
+    console.log(res.data);
+    if(res.data.status === true) {
+      dispatch(setAlert('Login Successful', 'success'));
+      dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+    // dispatch(loadUser());
+    dispatch(window.location.href = '/success');
+    } else {
+      dispatch(setAlert(res.data[0], 'error'));
+    }
+      
+    
+  } catch(error) {
+    console.log(error)
+    // dispatch(setAlert(error, 'error'))
+  //   error.map(sweetItem => {
+  //     return dispatch(setAlert(sweetItem, 'error'))
+  // })
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
+
+// Logout /Clear Profile
+
+export const logout = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: LOGOUT });
+  dispatch(window.location.href='/');
+};
