31ff8b67dbf15208ec8e77f6fcecacc569e73476
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-03-31 14:07:51 +0100

    updated the base routes

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -12,7 +12,7 @@ import {
 import setAuthToken from '../Utils/setAuthToken';
 import { setAlert } from './alert';
 
-const url = 'https://paytax-app.herokuapp.com/api/v1/user';
+const url = 'https://paytax-app.herokuapp.com/api/v1/users';
 
 export const loadUser = () => async dispatch => {
   
